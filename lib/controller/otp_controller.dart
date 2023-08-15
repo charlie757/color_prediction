56e801e5242d67cc080d87555b7756a8add334c1
Ravi <rraviggupta15@gmail.com>
2023-08-15 10:44:46 +0530

    auth api

--- lib/controller/otp_controller.dart
+++ lib/controller/otp_controller.dart
@@ -1,15 +1,20 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:color_demo/api/apiconfig.dart';
+import 'package:color_demo/config/routes.dart';
+import 'package:color_demo/controller/register_controller.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 
 class OtpController extends GetxController {
+  final registerController = Get.put(RegisterController());
+  final isLoading = false.obs;
   final otpController = TextEditingController();
   final formKey = GlobalKey<FormState>();
   final counter = 0.obs;
   Timer? timer;
-
-
 
   @override
   void onInit() async {
@@ -24,6 +29,7 @@ class OtpController extends GetxController {
       counter.value > 0 ? counter.value-- : timer.cancel();
     });
   }
+
   @override
   void dispose() {
     timer?.cancel();
@@ -34,5 +40,52 @@ class OtpController extends GetxController {
     otpController.text = "";
   }
 
+  checkValidation() async {
+    if (formKey.currentState!.validate()) {
+      verifyOtpApiFunction();
+    }
+  }
 
+  verifyOtpApiFunction() async {
+    isLoading.value = true;
+    var body = json.encode({
+      "mobile": "+91${registerController.numberController.text}",
+      "device_token": "test123123",
+      'otp': otpController.text
+    });
+    final response = await ApiConfig.post(
+        body: body, url: ApiConfig.verifyOtpUrl, useAuthToken: false);
+    isLoading.value = false;
+    if (response != null && response['success'] == true) {
+      response['user']['is_register'] != null &&
+              !response['user']['is_register']
+          ? createAccountApiFunction()
+          : null;
+    } else {
+      EasyLoading.showToast(response['message'].toString(),
+          toastPosition: EasyLoadingToastPosition.bottom);
+    }
+  }
+
+  createAccountApiFunction() async {
+    isLoading.value = true;
+    var body = json.encode({
+      "email": registerController.emailController.text.toString(),
+      "mobile": "+91${registerController.numberController.text}",
+      "password": registerController.passwordController.text.toString(),
+      "device_token": "test123123"
+    });
+    final response = await ApiConfig.post(
+        body: body, url: ApiConfig.signUpUrl, useAuthToken: false);
+    isLoading.value = false;
+    if (response != null && response['success'] == true) {
+      Get.offAllNamed(AppRoutes.dashboard);
+      EasyLoading.showToast(response['message'].toString(),
+          toastPosition: EasyLoadingToastPosition.bottom);
+    } else {
+      EasyLoading.showToast(response['message'].toString(),
+          toastPosition: EasyLoadingToastPosition.bottom);
+      // errorSnackBar('Error', response['message']);
+    }
+  }
 }
